c0c892d8c4d1a548fbe6ea40472220370c3d9a3d
victhor409 <victhor409@gmail,com>
2020-09-28 21:13:19 -0300

    hash map exemplo 2

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,31 +1,49 @@
 package application;
 
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
+		Scanner teclado = new Scanner(System.in);
+		
 		HashMap<Integer, String> produtos = new HashMap<>();
 		
-		//inserindo os valores das variaveis usando a palavra put
-		produtos.put(1011, "teclado");
-		produtos.put(2033,"mouse");
+		int opcao;
 		
-		//get retorna o valor da chave atribuida
-		System.out.println(produtos.get(1011));
-		
-		//percorre os valores do mapeamento 
-		for (String x : produtos.values()) {
-			System.out.println(x);
+		do {
+			System.out.println("Digite 1 para incluir, "+ "2 para consultar "+"0 para sair");
+			opcao= teclado.nextInt();
 			
+			switch(opcao) {
+			
+			//primeiro case inclui o codigo e o produto com a palavra put
+			case 1:
+				System.out.println("Código do novo produto");
+				int codigoN = teclado.nextInt();
+				System.out.println("Nome do Novo Produto");
+				String nomeN = teclado.next();
+				produtos.put(codigoN, nomeN);
+				break;
+				
+				//segundo case quando digitado a opcao 2, ele pede o codigo esse codigo e guardado com a palavra get
+				//e faz a comparação do codigo referencia ao priduto pertecente aquele codigo 
+			case 2:
+				System.out.println("Digite o codigo");
+				int codigo = teclado.nextInt();
+				String nome = produtos.get(codigo);
+				if(nome != null) {
+					
+					System.out.println(nome);
+					break;
+					
+				}
+			
+			}
 			
 		}
-		
-		//remove os objetos
-		produtos.remove(1011);
-		
-		
-	}
-
+		while(opcao!= 0);
+	}		
 }
